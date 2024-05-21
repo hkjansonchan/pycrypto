c429563e3eb966ce51d537854c2cb9c132779ebe
Janson <hkjansonchan@gmail.com>
2024-05-21 22:46:59 +0800

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import os
 
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential  # type: ignore
-from keras.layers import Dense, LSTM, Dropout, Input  # type: ignore
 from discord_webhook import DiscordWebhook
+
 
 
 def dcsend(url: str, mes: str):
@@ -21,3 +17,4 @@ try:
     os.system("fetch.py")
 except Exception as e:
     print(f"Error: {e}")
+
